010288159e4ee1d9ad0d6ff890ef62491505c791
HyunJi-2 <aaa9013@gmail.com>
2019-02-08 14:43:30 +0900

    Updates

---
+++ precache-manifest.ebd1f643310b3f889b5bf061eda2b643.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49e9a47ee01de4ef9f54",
+    "url": "/hi-test/static/js/runtime~main.49e9a47e.js"
+  },
+  {
+    "revision": "6390999b3c7d60b3aec6",
+    "url": "/hi-test/static/js/main.6390999b.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/hi-test/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "6390999b3c7d60b3aec6",
+    "url": "/hi-test/static/css/main.0d14e398.chunk.css"
+  },
+  {
+    "revision": "94787f59e051eccd43540ba09eefff66",
+    "url": "/hi-test/index.html"
+  }
+];
